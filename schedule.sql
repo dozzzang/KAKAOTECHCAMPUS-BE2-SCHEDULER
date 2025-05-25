70825f1afec5da972d6d8e64a728c785e0097923
Dohyun <sks020k@naver.com>
2025-05-25 13:22:02 +0900

    modify : sql by 연관관계

--- schedule.sql
+++ schedule.sql
@@ -1,10 +1,19 @@
 USE schedule;
+CREATE TABLE user
+(
+    user_id BIGINT AUTO_INCREMENT PRIMARY KEY COMMENT '작성자 식별자',
+    password VARCHAR(100) NOT NULL COMMENT '비밀번호',
+    username VARCHAR(100) NOT NULL COMMENT '작성자명',
+    email VARCHAR(100) COMMENT '이메일',
+    create_date DATETIME NOT NULL COMMENT '사용자 생성일',
+    update_date DATETIME NOT NULL COMMENT '마지막 수정일'
+);
 CREATE TABLE schedule
 (
     schedule_id BIGINT AUTO_INCREMENT PRIMARY KEY COMMENT '일정 식별자',
     content_todo VARCHAR(100) NOT NULL COMMENT '할일',
-    username VARCHAR(100) NOT NULL COMMENT '작성자명',
-    password VARCHAR(100) NOT NULL COMMENT '비밀번호',
     create_date DATETIME NOT NULL COMMENT '작성일',
-    update_date DATETIME NOT NULL COMMENT '수정일'
+    update_date DATETIME NOT NULL COMMENT '수정일',
+    user_id BIGINT NOT NULL COMMENT '작성자 식별자',
+    foreign key (user_id) REFERENCES user(user_id)
 );
